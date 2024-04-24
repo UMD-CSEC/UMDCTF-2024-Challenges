bd6d1a2463fb662ab74f7f9fe2410db9aea708ed
obafgkmdh <22942098+obafgkmdh@users.noreply.github.com>
2024-04-24 03:24:07 -0400

    add triple_des challenge

---
+++ crypto/triple_des/README.md
@@ -0,0 +1,12 @@
+# Triple DES
+i'll think of a description later
+
+## Build Instructions
+needs dockerfile
+
+## Files for CTFd
+- `tripledes.py` 
+
+## Flag
+
+`UMDCTF{padding_oracle_with_extra_steps?}`
---
+++ crypto/triple_des/tripledes.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+from os import urandom
+from Crypto.Cipher import DES
+from Crypto.Util.Padding import pad, unpad
+
+key = bytes.fromhex(open("key.txt", "r").read())
+flag = open("flag.txt", "rb").read()
+
+enc = pad(flag, 8)
+for i in range(3):
+	cipher = DES.new(key, DES.MODE_CBC)
+	enc = cipher.iv + cipher.encrypt(enc)
+print("Here's the encrypted flag:", enc.hex())
+
+while 1:
+	print("Give us an encrypted text and we'll tell you if it's valid!")
+	enc = input()
+	try: enc = bytes.fromhex(enc)
+	except:
+		print("no")
+		break
+	if len(enc) % 8 != 0 or len(enc) < 32:
+		print("no")
+		break
+	try:
+		for i in range(3):
+			iv, enc = enc[:8], enc[8:]
+			cipher = DES.new(key, DES.MODE_CBC, iv=iv)
+			enc = cipher.decrypt(enc)
+		dec = unpad(enc, 8)
+		print("yes")
+	except:
+		print("no")
